75d218187d5110c7605359f97ae63c4e0ed93717
Aaron <achan101@live.com>
2018-03-28 21:51:55 +1000

    First Commit

---
+++ home/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from .models import *
+# Register your models here.
+
+admin.site.register(Teacher)
+admin.site.register(Student)
+admin.site.register(Lesson)
+admin.site.register(Instrument)
+admin.site.register(TeacherQualification)
+admin.site.register(TeacherInstrument)
+admin.site.register(TeacherLanguage)
+admin.site.register(TeacherPhone)
+admin.site.register(StudentPhone)
+admin.site.register(Parent)
+admin.site.register(ParentPhone)
---
+++ home/models.py
@@ -0,0 +1,124 @@
+from django.db import models
+
+# Create your models here.
+class Teacher(models.Model):
+    GENDERS = (
+        ( 'M', 'Male'),
+        ('F', 'Female')) # Choices for gender, first entry is what goes into DB, second is what is displayed
+
+    firstName = models.CharField(max_length=30)
+    lastName = models.CharField(max_length=30)
+    gender = models.CharField(max_length=1, choices=GENDERS)
+    studentFeedback = models.TextField()
+    email = models.EmailField(max_length=75)
+    DoB = models.DateField()
+    facebookID = models.IntegerField()
+
+    def __str__(self): # Displays firstName + lastName
+        return self.id + ' - ' + self.firstName + ' - ' + self.lastName
+
+class Student(models.Model):
+    GENDERS = (
+    ('M', 'Male'),
+    ('F', 'Female'))
+
+    firstName = models.CharField(max_length=30)
+    lastName = models.CharField(max_length=30)
+    gender = models.CharField(max_length=1, choices=GENDERS)
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+    DoB = models.DateField()
+    address = models.TextField()
+    facebookID = models.IntegerField()
+
+
+    def __str__(self): # Displays firstName + lastName
+        return self.id + ' - ' + self.firstName + ' - ' + self.lastName
+
+
+class Lesson(models.Model):
+    DURATION_1 = 30
+    DURATION_2 = 60
+
+    DURATIONS = (
+        (DURATION_1, "30min"),
+        (DURATION_2, "60min")
+    )
+
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+    startDate = models.DateField()
+    endDate = models.DateField()
+    duration = models.IntegerField(choices=DURATIONS)
+    students = models.ManyToManyField(Student)# students can have many lessons vice versa
+
+    def __str__(self): # Displays firstName + lastName
+        return self.id + ' - ' + self.teacher
+
+class Instrument(models.Model):
+    CONDITIONS = (
+        ('N', 'New'),
+        ('E', 'Excellent'),
+        ('G', 'Good'),
+        ('R', 'Repair'),
+        ('D', 'Discard')
+    )
+
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    type = models.CharField(max_length=50)  #+ add NULL as well
+    costofHire = models.IntegerField()
+    purchaseCost = models.IntegerField()
+    condition = models.CharField(max_length=1, choices=CONDITIONS)
+
+    def __str__(self): #Displays instrument ID + type + condition
+        return self.id + ' - ' + self.type + ' - ' + self.condition
+
+
+class Parent (models.Model):
+    firstName = models.CharField(max_length=30)
+    lastName = models.CharField(max_length=30)
+    students = models.ManyToManyField(Student)#Parents can have many children vice versa
+
+    def __str__(self): #Displays firstName, lastName
+        return self.firstName + ' - ' + self.lastName
+
+
+class TeacherQualification (models.Model):
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+    qualification = models.CharField(max_length=50)
+
+    def __str__(self): # Display Teacher ID + Qualification
+        return self.teacher + ' - ' + self.qualification
+
+class TeacherInstrument (models.Model):
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+    instrumentPlayed = models.CharField(max_length=50)
+
+    def __str__(self): #Display teacher ID, instrument played
+        return self.teacher + ' - ' + self.instrumentPlayed
+
+class TeacherLanguage (models.Model): #Display Teacher ID, language spoken
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+    languageSpoken = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.teacher + ' - ' + self.languageSpoken
+
+class TeacherPhone (models.Model):
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+    phoneNumber = models.CharField(max_length=10) #without country code e.g. +61
+
+    def __str__(self):
+        return self.teacher + ' - ' + self.phoneNumber
+
+class StudentPhone (models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    phoneNumber = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.student + ' - ' + self.phoneNumber
+
+class ParentPhone (models.Model):
+    parent = models.ForeignKey(Parent, on_delete=models.CASCADE)
+    phoneNumber = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.parent + ' - ' + self.phoneNumber
