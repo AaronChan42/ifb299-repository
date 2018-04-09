a294b31bebdf634fe8645f3f1a92b86a3ffb7689
Aaron <achan101@live.com>
2018-04-09 20:38:07 +1000

    Added base template and about

--- home/urls.py
+++ home/urls.py
@@ -3,6 +3,10 @@
 from django.conf.urls import url
 from . import views
 
+app_name = 'home'
+
 urlpatterns = [
-    url(r'^$', views.index, name='index') # /home/
+    url(r'^$', views.index, name='index'), # /home/
+    url(r'about/', views.about, name = 'about'), # /home/about
 ]
+
--- home/views.py
+++ home/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, 'home/index.html') #add context argument/parameter if you are grabbing something from db
+
+def about(request):
+    return render(request, 'home/about.html')
