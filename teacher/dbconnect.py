096307f7a8393b546493b78ea95e30ad46a78b07
lchjames <36805669+lchjames@users.noreply.github.com>
2018-05-16 12:05:01 +1000

    Update dbconnect.py

--- teacher/dbconnect.py
+++ teacher/dbconnect.py
@@ -3,7 +3,7 @@ config = {
 'host':'127.0.0.1',
 'port':3306,
 'user':'root',
-'password':'',
+'password':'password',
 #'db':'musicschool',
 #'charset':'utf8mb4',
 'cursorclass':pymysql.cursors.DictCursor,
