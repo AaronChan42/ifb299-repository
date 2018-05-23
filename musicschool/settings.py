fb735e4c252db789053df9eb3bfd51cd628bcba6
Aaron <achan101@live.com>
2018-05-23 18:12:55 +1000

    Overrided admin redirect when logging out

--- musicschool/settings.py
+++ musicschool/settings.py
@@ -31,15 +31,16 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'home',
+    'student',
+    'teacher',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'home',
-    'student',
-    'teacher',
+
 ]
 
 MIDDLEWARE = [
